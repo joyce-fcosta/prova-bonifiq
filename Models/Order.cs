8164f6bb062e9ef3f4e42fd981a706c3ff1ccee2
joyce costa <joyced_fcosta@hotmail.com>
2025-08-20 15:31:19 -0300

    feat: Refatora o modelo de dados para relações e encapsulamento

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,11 +1,21 @@
 ﻿namespace ProvaPub.Models
 {
-	public class Order
-	{
-		public int Id { get; set; }
-		public decimal Value { get; set; }
-		public int CustomerId { get; set; }
-		public DateTime OrderDate { get; set; }
-		public Customer Customer { get; set; }
-	}
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Customer? Customer { get; set; }
+
+        public static Order Create(decimal value, int customerId)
+        {
+            return new Order
+            {
+                CustomerId = customerId,
+                Value = value,
+                OrderDate = DateTime.UtcNow
+            };
+        }
+    }
 }
