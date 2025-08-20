82fc0cc609d4bfa9aff48c6f7d765c0bd4d57247
joyce costa <joyced_fcosta@hotmail.com>
2025-08-20 08:35:07 -0300

    feat: Adiciona método de extensão para paginação assíncrona

---
+++ Helpers/PaginationHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaPub.Models;
+
+namespace ProvaPub.Helpers
+{
+    public static class PaginationHelper
+    {
+        public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> query, int page, int pageSize = 10)
+        {
+            var totalCount = await query.CountAsync(); // Use CountAsync()
+            var items = await query.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync(); // Use ToListAsync()
+
+            return new PagedList<T>
+            {
+                Items = items,
+                TotalCount = totalCount,
+                HasNext = totalCount > page * pageSize
+            };
+        }
+    }
+}
